1a5abbd0ae5fc575c141241d04f5dd88133a0bf2
encleine <encleine@gmail.com>
2025-01-21 12:21:37 +0300

    changed keymap

--- lua/custom/configs/oil.lua
+++ lua/custom/configs/oil.lua
@@ -11,4 +11,4 @@ require("oil").setup {
 	}
 }
 
-vim.keymap.set("n", "<space>-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
