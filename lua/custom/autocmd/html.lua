ff843dd74df12a3c13bad4df96613b19a45fe4c3
encleine <encleine@gmail.com>
2025-05-18 17:11:29 +0300

    new autocmd for html

---
+++ lua/custom/autocmd/html.lua
@@ -0,0 +1,11 @@
+-- [[ added comment string for sql files ]]
+local group = vim.api.nvim_create_augroup('htlmTabs', { clear = true })
+
+vim.api.nvim_create_autocmd('BufEnter', {
+	callback = function()
+		vim.opt["shiftwidth"] = 4
+		vim.opt["tabstop"]    = 4
+	end,
+	group = group,
+	pattern = '*.html',
+})
