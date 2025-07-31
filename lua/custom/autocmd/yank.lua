4dc82186ae36b09e54cc4d1795150df0ee9b8a26
encleine <encleine@gmail.com>
2025-07-31 14:56:38 +0300

    removed deprecated function

--- lua/custom/autocmd/yank.lua
+++ lua/custom/autocmd/yank.lua
@@ -2,7 +2,7 @@
 local group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
 	callback = function()
-		vim.highlight.on_yank { timeout = 200, higroup = "lualine_a_visual" }
+		vim.hl.on_yank { timeout = 200, higroup = "lualine_a_visual" }
 	end,
 	group = group,
 	pattern = '*',
