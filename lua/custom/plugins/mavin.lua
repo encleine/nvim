24e09ea2b66ec597943f4f0cf0b31fc177b2fe2b
encleine <encleine@gmail.com>
2025-09-01 23:35:55 +0300

    added a toggle for supermaven in settings

--- lua/custom/plugins/mavin.lua
+++ lua/custom/plugins/mavin.lua
@@ -1,5 +1,11 @@
+local settings = require("custom.setting.json").settings
+
+if not settings.supermaven then
+	return {}
+end
+
 return {
-	event = 'BufReadPre',
+	event = "BufReadPre",
 	"supermaven-inc/supermaven-nvim",
 	config = function()
 		require("supermaven-nvim").setup({})
