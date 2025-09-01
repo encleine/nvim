7c1d1d16c1ea053ac08befdb5162b0b2ec5ae516
encleine <encleine@gmail.com>
2025-09-01 23:24:50 +0300

    dressing + mini pairs + ts auto tags

---
+++ lua/custom/plugins/tsAutoTag.lua
@@ -0,0 +1,13 @@
+return {
+	"windwp/nvim-ts-autotag",
+	-- event = "BufReadPre",
+	ft = {
+		"html",
+		"javascript",
+		"typescript",
+		"typescriptreact",
+		"javascriptreact",
+	},
+
+	opts = {},
+}
