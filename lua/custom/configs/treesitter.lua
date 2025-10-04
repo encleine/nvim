208ee74637ca7bd827d2ae2854e1a927ccf22596
encleine <encleine@gmail.com>
2025-10-04 17:21:33 +0300

    checking treesitter capture groups under a flag

--- lua/custom/configs/treesitter.lua
+++ lua/custom/configs/treesitter.lua
@@ -1,3 +1,29 @@
+local settings = require("custom.setting.json").settings()
+if not settings then
+	return
+end
+
+local selection_modes = {
+	["@parameter.outer"] = "v", -- charwise
+	["@function.outer"] = "V", -- linewise
+	["@class.outer"] = "<c-v>", -- blockwise
+}
+
+local keymaps = {
+
+	["af"] = "@function.outer",
+	["if"] = "@function.inner",
+	["ac"] = "@class.outer",
+
+	["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+	["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+}
+
+if not settings.experimental then
+	keymaps = {}
+	selection_modes = {}
+end
+
 require("nvim-treesitter.configs").setup({
 	-- Add languages to be installed here that you want installed for treesitter
 	sync_install = false,
@@ -32,6 +58,10 @@ require("nvim-treesitter.configs").setup({
 		select = {
 			enable = true,
 			lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+
+			-- experimental
+			keymaps = keymaps,
+			selection_modes = selection_modes,
 		},
 
 		swap = {
