2df859e64debceda81e30d115e8905d781a1a885
encleine <encleine@gmail.com>
2025-10-04 16:28:47 +0300

    changed column text for git signes

--- lua/custom/plugins/gitsigns.lua
+++ lua/custom/plugins/gitsigns.lua
@@ -1,16 +1,16 @@
 return {
-	'lewis6991/gitsigns.nvim',
+	"lewis6991/gitsigns.nvim",
 	event = "BufReadPre",
 	opts = {
 		-- See `:help gitsigns.txt`
 		signs = {
-			add          = { text = '┃' },
-			change       = { text = '┃' },
-			delete       = { text = '┃' },
-			topdelete    = { text = '┃' },
-			changedelete = { text = '┃' },
+			add = { text = "┃" },
+			change = { text = "┃" },
+			delete = { text = "┇" },
+			topdelete = { text = "┇" },
+			changedelete = { text = "┇" },
 		},
 		current_line_blame = true,
-		current_line_blame_formatter = '<author_time:%Y-%m-%d> <author> - <summary>',
+		current_line_blame_formatter = "<author_time:%Y-%m-%d> <author> - <summary>",
 	},
 }
